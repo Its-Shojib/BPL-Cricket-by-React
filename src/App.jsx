af1ecc46a8a0b0de64104d65e89909ce19710b2d
Its_Shojib <mdshojib922@gmail.com>
2023-09-13 21:06:43 +0600

    Header Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Header from "./components/Header/Header"
 function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <Header></Header>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <div className="max-w-screen-xl mx-auto bg-white px-4 md:px-8 lg:px-12">
+            <div className="text-center text-green-600 flex justify-center items-center gap-5">
+                <img className="w-52" src="../../../public/logo.jpg" alt="" />
+                <div>
+                    <h1 className="text-5xl font-bold">Bangladesh Premier League</h1>
+                    <h3 className="text-2xl font-semibold pt-2">This Player are avaliable for your team</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
